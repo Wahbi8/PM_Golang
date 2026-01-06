1ee982ea6085a9b38b1a5fbff840a3fe7fc7cad6
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-01-06 21:41:21 +0100

    commit

--- Services/SendEmail.go
+++ Services/SendEmail.go
@@ -1,7 +1,19 @@
+
 package Services
 
+import "github.com/resend/resend-go/v2"
 
+func SendEmail() {
+    apiKey := "re_UgRSdtU7_74vHTKr3GAv3DcUcb2dBkJsp"
 
-func SendEmail() error {
-	
+    client := resend.NewClient(apiKey)
+
+    params := &resend.SendEmailRequest{
+        From:    "onboarding@resend.dev",
+        To:      []string{"wahbi.oussama08@gmail.com"},
+        Subject: "Hello World",
+        Html:    "<p>Congrats on sending your <strong>first email</strong>!</p>",
+    }
+
+    sent, err := client.Emails.Send(params)
 }

bf3a5bd2372b6a6f46ff58da3afb9cbaf5d408c9
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-01-08 22:04:16 +0100

    -m

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.4
 require github.com/resend/resend-go/v2 v2.28.0
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ infrastructure/dbConnection.go
@@ -0,0 +1,7 @@
+package infrastructure
+
+import "github.com/lib/pq"
+
+func Connection() {
+	connStr := "postgres://postgres:Postgresqlaccount1@localhost:5432/image_enhancer?sslmode=disable"
+}
